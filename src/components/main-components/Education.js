b1d268ae5bb15eec2715f172a7e411dc6c526918
Sean Boonzaier <boonzaiersa@gmail.com>
2022-09-25 10:50:53 +0200

    Add states to Education component

--- src/components/main-components/Education.js
+++ src/components/main-components/Education.js
@@ -2,6 +2,15 @@ import { useState } from 'react'
 
 function Education () {
 
+    const[isSaved, setIsSaved] = useState(false)
+    const[educationInfo, setEducationInfo] = useState (
+        {
+            institution: '',
+            qualification:'',
+            yearStarted:'',
+            yearFinished:'',
+        }
+    )
 
     return (
         <div>
